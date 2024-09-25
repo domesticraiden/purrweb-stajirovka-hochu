2c4eb2898523f303b6d220300d975e35f84d7643
domesticraiden <thecreatorcouldnt@gmail.com>
2024-09-25 11:56:49 +0600

    finish site

--- script.js
+++ script.js
@@ -1,73 +1,100 @@
-const body = document.querySelector("body");
+const menu = document.querySelector(".header__menu");
+const openMenu = document.querySelector(".burger");
+const closeMenu = document.querySelector(".header__close");
 
-const headerMenu = document.querySelector(".header__menu");
-const openMenuButton = document.querySelector(".burger");
-const closeMenuButton = document.querySelector(".header__close");
-openMenuButton.addEventListener("click", openHeaderMenu);
-closeMenuButton.addEventListener("click", closeHeaderMenu);
+const enter = document.querySelector(".enter");
+const enterBtn = document.querySelector(".enter__btn");
+const openEnter = document.querySelectorAll(".open");
+const closeEnter = document.querySelector(".enter__close");
 
-const dialog = document.querySelector(".dialog");
-const openDialogButton = document.querySelectorAll(".open");
-const closeDialogButton = document.querySelector(".dialog__close");
 const field = document.querySelectorAll(".field");
 const fieldRequired = document.querySelectorAll(".field-required");
-const fieldTitle = document.querySelectorAll(".dialog__form-title");
-const dialogRequired = document.querySelector(".dialog__required");
-const dialogBtn = document.querySelector(".dialog__btn");
-openDialogButton.forEach((item) =>
+const fieldTitle = document.querySelectorAll(".enter__form-title");
+const enterRequired = document.querySelector(".enter__required");
+
+const done = document.querySelector(".done");
+const doneBtn = document.querySelector(".done__btn");
+const closeDone = document.querySelector(".done__close");
+
+openMenu.addEventListener("click", () => {
+  Visible(menu);
+});
+
+closeMenu.addEventListener("click", () => {
+  Hidden(menu);
+});
+
+enter.addEventListener("cancel", () => {
+  closeDialog(enter);
+});
+
+enterBtn.addEventListener("click", () => {
+  closeDialog(enter);
+  openDialog(done);
+});
+
+openEnter.forEach((item) => {
   item.addEventListener("click", () => {
-    dialogBtn.disabled = true;
+    enterBtn.disabled = true;
     checkFieldOnOpen();
-    openDialog(dialog);
-  })
-);
-dialog.addEventListener("click", checkBackdrop);
-closeDialogButton.addEventListener("click", () => {
-  closeDialog(dialog);
+    openDialog(enter);
+  });
+});
+
+closeEnter.addEventListener("click", () => {
+  closeDialog(enter);
   checkFieldOnClose();
 });
+
+done.addEventListener("cancel", () => {
+  closeDialog(done);
+});
+
+doneBtn.addEventListener("click", () => {
+  closeDialog(done);
+});
+
+closeDone.addEventListener("click", () => {
+  closeDialog(done);
+});
+
 fieldRequired.forEach((item) => {
   item.addEventListener("blur", () => {
     checkField(item);
-    updateDialogBtn();
+    updateEnterBtn();
   });
 });
-dialogBtn.addEventListener("click", () => {
-  closeDialog(dialog);
-  checkFieldOnClose();
-  openDialog(done);
-});
-dialog.addEventListener("close", () => {
-  closeOnESC(dialog);
-  checkFieldOnClose();
+
+enter.addEventListener('click', (event) => {
+  if (event.target.nodeName === 'DIALOG') {
+    closeDialog(enter);
+  }
 });
 
-const done = document.querySelector(".done");
-const closeDoneButton = document.querySelector(".done__close");
-const doneBtn = document.querySelector(".done__btn");
-closeDoneButton.addEventListener("click", () => {
-  closeDialog(done);
-});
-doneBtn.addEventListener("click", () => {
-  closeDialog(done);
-})
-done.addEventListener("close", () => {
-  closeOnESC(done);
+done.addEventListener('click', (event) => {
+  if (event.target.nodeName === 'DIALOG') {
+    closeDialog(done);
+  }
 });
 
-function openHeaderMenu() {
-  headerMenu.style.display = "block";
-  body.style.overflow = "hidden";
+function Visible(item) {
+  item.style.display = "block";
+  document.body.style.overflow = "hidden";
 }
-function closeHeaderMenu() {
-  headerMenu.style.display = "none";
-  body.style.overflow = "";
+
+function Hidden(item) {
+  item.style.display = "none";
+  document.body.style.overflow = "";
 }
 
 function openDialog(item) {
   item.showModal();
-  item.style.display = "flex";
-  body.style.overflow = "hidden";
+  Visible(item);
+}
+
+function closeDialog(item) {
+  item.close();
+  Hidden(item);
 }
 
 function checkFieldOnOpen() {
@@ -78,37 +105,6 @@ function checkFieldOnOpen() {
   });
 }
 
-function checkBackdrop(event) {
-  let rect = event.target.getBoundingClientRect();
-
-  if (
-    rect.left > event.clientX ||
-    rect.right < event.clientX ||
-    rect.top > event.clientY ||
-    rect.bottom < event.clientY
-  ) {
-    closeDialog(dialog);
-    checkFieldOnClose();
-  }
-}
-function closeDialog(item) {
-  item.close();
-  item.style.display = "none";
-  body.style.overflow = "";
-}
-function checkFieldOnClose() {
-  field.forEach((item) => {
-    item.value = "";
-  });
-
-  fieldRequired.forEach((item) => {
-    item.style.borderColor = "#f1f1f1";
-
-    const requiredText = item.nextElementSibling;
-    requiredText.style.display = "none";
-    dialogRequired.style.display = "none";
-  });
-}
 function checkField(item) {
   const requiredText = item.nextElementSibling;
   if (item.value.length == 0) {
@@ -122,15 +118,26 @@ function checkField(item) {
   const allRed = Array.from(fieldRequired).every(
     (field) => field.style.borderColor === "var(--additional-02)"
   );
-  dialogRequired.style.display = allRed ? "block" : "none";
+  enterRequired.style.display = allRed ? "block" : "none";
 }
-function updateDialogBtn() {
+
+function updateEnterBtn() {
   const anyEmpty = Array.from(fieldRequired).some(
     (field) => field.value.length == 0
   );
-  dialogBtn.disabled = anyEmpty;
+  enterBtn.disabled = anyEmpty;
 }
 
-function closeOnESC(event) {
-  event.style.display = "none";
+function checkFieldOnClose() {
+  field.forEach((item) => {
+    item.value = "";
+  });
+
+  fieldRequired.forEach((item) => {
+    item.style.borderColor = "#f1f1f1";
+
+    const requiredText = item.nextElementSibling;
+    requiredText.style.display = "none";
+    enterRequired.style.display = "none";
+  });
 }
